60857bc9064eb6313d39156d8fde93c1b81d789d
Jacek Czerniachowicz <jacek.filip.czerniachowicz@gmail.com>
2022-10-16 12:49:17 +0200

    Game modified, now player can win

--- src/Game.java
+++ src/Game.java
@@ -11,20 +11,42 @@ public class Game {
         System.out.println(movieName);
 
         List<Character> playerGuesses = new ArrayList<>();
+        playerGuesses.add(' ');
 
 
+        int playerLife = 10;
+        boolean isRunning = true;
+
+        printWordState(movieName, playerGuesses);
+        while (true) {
+            getPlayerGuess(scanner, movieName, playerGuesses);
+            if(printWordState(movieName, playerGuesses)){
+                break;
+            }
+        }
+
+        System.out.println("You Win!");
     }
-    private static void printWordState(String movieName, List<Character> playerGuesses){
+    private static boolean printWordState(String movieName, List<Character> playerGuesses){
+        int correctCount = 0;
         for (int i = 0; i < movieName.length(); i++) {
             if (playerGuesses.contains(movieName.charAt(i))) {
                 System.out.print(movieName.charAt(i));
-            }if (movieName.charAt(i) == ' ') {
+                correctCount++;
+            }else if (movieName.charAt(i) == ' ') {
                 System.out.print(" ");
             }else {
                 System.out.print("_");
             }
         }
         System.out.println();
+        return (movieName.length() == correctCount);
+    }
+
+    private static void getPlayerGuess(Scanner scanner, String movieName, List<Character> playerGuesses){
+        System.out.println("Please enter a letter ");
+        String letterGuess = scanner.nextLine();
+        playerGuesses.add(letterGuess.charAt(0));
     }
 
 }
